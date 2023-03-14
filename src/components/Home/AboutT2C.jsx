7eaecc7b589dfbea4c4acb7cbb72e407a285857f
Ashutosh Jha <ashutoshj887@gmail.com>
2023-03-14 20:16:08 +0530

    add: assets

--- src/components/Home/AboutT2C.jsx
+++ src/components/Home/AboutT2C.jsx
@@ -25,7 +25,7 @@ const AboutT2C = () => {
 
       <div className="flex flex-col items-center justify-center">
         <Image
-          src="/t2c.jpg"
+          src="/logo.jpg"
           width={150}
           height={150}
           alt="t2c-circular-logo"
